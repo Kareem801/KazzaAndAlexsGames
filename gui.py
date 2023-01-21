ba293e44dc055f6ff155e34049cf32fbfc4f4be3
Kareem801 <103330503+Kareem801@users.noreply.github.com>
2023-01-21 19:15:35 +0000

    made pong, started trying sound effects

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pacman
 import snake
+import pong
 import pyautogui
 mainWindow = Tk()
 width, height = pyautogui.size()
@@ -12,10 +13,12 @@ mainWindow.config(menu=mn)
 
 pacmanButton = Button(mainWindow, text="Pacman", width=50, height=20, command=lambda: pacman.run())
 snakeButton = Button(mainWindow, text="Snake", width=50, height=20, command=lambda: snake.run())
+pongButton = Button(mainWindow, text="Pong", width=50, height=20, command=lambda: pong.run())
 
 def buttonClear():
     pacmanButton.forget()
     snakeButton.forget()
+    pongButton.forget()
 
 def pacmanGame():
     buttonClear()
@@ -25,12 +28,16 @@ def snakeGame():
     buttonClear()
     snakeButton.pack()
 
+def pongGame():
+    buttonClear()
+    pongButton.pack()
 
 
 gameMenu = Menu(mn)
 mn.add_cascade(label='Games', menu=gameMenu)
 gameMenu.add_command(label="Pacman", command=pacmanGame)
 gameMenu.add_command(label="Snake", command=snakeGame)
+gameMenu.add_command(label="Pong", command=pongGame)
 
 
 mainWindow.mainloop()
